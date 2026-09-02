67c403478c8c499c04e94660e90c91c816e3f0a0
Prasad Somani <prasadjsomani2005@gmail.com>
2026-09-02 22:17:23 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int* ptr = NULL;
+    cout << *ptr; 
+
+    for(int i = 0; i < 10; i--) { 
+        cout << "Hello";
+    }
+    return 0;
+}
